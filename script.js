f1deefecbe76c7d1e920afb9d81eedf9217c0831
lautato15 <lautato15@gmail.com>
2024-04-04 22:42:50 -0300

    Script

---
+++ script.js
@@ -0,0 +1,24 @@
+function countVowels(str) {
+  let counter = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    )
+      counter += 1;
+  }
+  return counter;
+}
+function countVowels2(str) {
+  let vocals = ["a", "e", "i", "o", "u"];
+  let counter = 0;
+  for (vocal of vocals) {
+    for (var i = 0; i < str.length; i++) {
+      if (vocal === str[i]) counter += 1;
+    }
+  }
+  return counter;
+}
